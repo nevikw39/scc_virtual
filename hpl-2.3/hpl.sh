7efe0a499836eb390db69c2b5ab09244be686a37
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-04-27 13:53:49 +0800

    Update HPL slurm script

--- hpl-2.3/hpl.sh
+++ hpl-2.3/hpl.sh
@@ -4,12 +4,11 @@
 #SBATCH -A ACD110018
 #SBATCH -o hpl_output.txt
 #SBATCH -e hpl_error.txt
-#SBATCH -p ct56
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=56
+#SBATCH -p ct224
+#SBATCH -n 112
 #SBATCH --ntasks-per-core=2
 #SBATCH --hint=multithread
 
 cd bin/Linux_PII_CBLAS/
 
-srun -n 56 ./xhpl
+srun -n 112 ./xhpl
